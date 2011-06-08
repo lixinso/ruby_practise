d0c9f42f05e82d83f2c27cd6e2cd0f6fe0ab674a
lixinso <lixinso@gmail.com>
2011-06-08 14:05:46 +0800

    ckeditor usage and filter

---
+++ ckeditor_test/ckeditor/plugins/insertResolved/plugin.js
@@ -0,0 +1,33 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.add( 'insertResolved',
+{
+	requires : [ 'selection' ],
+
+	init : function( editor )
+	{
+		editor.addCommand( 'insertResolved', CKEDITOR.plugins.insertResolved.commands.insertResolved );
+		editor.ui.addButton( 'insertResolved',
+			{
+				label : 'Insert Resolved',
+				command : 'insertResolved'
+			});
+	}
+});
+
+CKEDITOR.plugins.insertResolved =
+{
+	commands :
+	{
+		insertResolved :
+		{
+			exec : function( editor )
+			{
+			  editor.insertHtml('<em>Resolved,</em>&nbsp;');
+			}
+		}
+	}
+};
