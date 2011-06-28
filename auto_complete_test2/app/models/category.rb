2fcac77c6768266d317f3db5b7fc2c9d1f133897
lixinso <lixinso@gmail.com>
2011-06-28 08:33:38 +0800

    can automaticly generate id now

--- auto_complete_test2/app/models/category.rb
+++ auto_complete_test2/app/models/category.rb
@@ -1,10 +1,9 @@
 class Category < ActiveRecord::Base
-  attr_accessor :id_name
+  #attr_accessor :id_name
   
    def id_name
-    #self.id + "%" + self.name
-    "dddddddddd"
-  end
+     self.id + "%" + self.name
+   end
   def id_name(sth)
     
   end
--- auto_complete_test2/public/javascripts/products.js
+++ auto_complete_test2/public/javascripts/products.js
@@ -1,11 +1,12 @@
 function updateProduct(e){
-	//var str = e.value;
-	//var arr = str.split(/%/);
-	//e.value = arr[0];
-	//$("category_id").value = arr[1];
+	var str = e.value;
+        
+	var arr = str.split(/:/);
+	e.value = arr[1];
+	$("product_category_id").value = arr[0];
         //$("category_id").value = 2;
-        alert(e);
-        alert(e.value)
-        $("product_category_id").value = 2;
+        alert(e.value);
+        alert($("product_category_id").value);
+        //$("product_category_id").value = 2;
         alert('after update');
 }
